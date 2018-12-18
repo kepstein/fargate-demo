dadcabc2d89a3244b42daf233a4a34b45fbb64fa
Kevin Epstein <kevin@epstein.co.za>
2018-12-17 19:47:10 -0800

    Updated deploy script to include parameters

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,10 @@
 
 aws cloudformation create-stack \
 --stack-name $1 \
---template-body file://CloudFormation/fargate-demo.yml \
+--template-body file://fargate_demo.cfn/templates/fargate-demo.yml \
+--parameters \
+ParameterKey=SubnetAZ1,ParameterValue=subnet-0f3de278 \
+ParameterKey=SubnetAZ2,ParameterValue=subnet-638e773a \
+ParameterKey=SubnetAZ3,ParameterValue=subnet-64dec74c \
+ParameterKey=RepoName,ParameterValue=fargate-demo \
 --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM

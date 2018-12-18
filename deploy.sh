14f30147dbb1c53c055d55d2fe4efd1f39cfcd93
Kevin Epstein <kevin@epstein.co.za>
2018-12-17 21:58:58 -0800

    Cleaned up template - removed parameters that are not needed.

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,5 @@
 aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://fargate_demo.cfn/templates/fargate-demo.yml \
---parameters \
-ParameterKey=SubnetAZ1,ParameterValue=subnet-0f3de278 \
-ParameterKey=SubnetAZ2,ParameterValue=subnet-638e773a \
-ParameterKey=SubnetAZ3,ParameterValue=subnet-64dec74c \
-ParameterKey=RepoName,ParameterValue=fargate-demo \
+--parameters ParameterKey=RepoName,ParameterValue=fargate-demo \
 --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM
